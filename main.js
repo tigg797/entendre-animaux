356e29efd32361dba733dbfc690d1b254d9d3fe7
tigg797 <98042739+tigg797@users.noreply.github.com>
2024-07-13 18:40:44 +0100

    Part 2 Finale

--- main.js
+++ main.js
@@ -5,9 +5,28 @@ https://en.wikipedia.org/wiki/Dog*/
 var animale = ""
 
 document.getElementById("the_anchor").href="http://en.wikipedia.org/wiki/"+animale;
-if(animale==="") {
+if(animale=="") {
     document.getElementById("learnMore").style.opacity=0.4
 } else {
     document.getElementById("learnMore").style.opacity=1
 }
 
+function startClassification() {
+    document.getElementById("iDisappear").style.display='none';
+    var smt = navigator.mediaDevices
+    smt.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier(
+    'https://teachablemachine.withgoogle.com/models/M8jw4szDY/model.json'
+    , modelReady);
+}
+
+function myFunction() {
+    // Copy the text inside the text field
+    navigator.clipboard.writeText("https://tigg797.github.io/entendre-animaux/");
+    // Alert the copied text
+    alert("Copied the link: tigg797.github.io/entendre-animaux");
+  }
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
